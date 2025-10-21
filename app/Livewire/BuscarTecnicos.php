96c5417ebb30a7ffd5d9d194ca9dbc89c43f49fa
OCTAVIO ESCALONA LOPEZ <oescalona16@alumnos.uaq.mx>
2025-10-20 22:01:17 -0600

    Commit inicial

---
+++ app/Livewire/BuscarTecnicos.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class BuscarTecnicos extends Component
+{
+    public function render()
+    {
+        return view('livewire.buscar-tecnicos');
+    }
+}
---
+++ resources/views/components/application-logo.blade.php
@@ -0,0 +1,12 @@
+<div class="flex items-center">
+    <!-- Logo -->
+    <div class="flex-shrink-0">
+        <img src="{{ asset('images/logo.png') }}" alt="Bridgely" class="h-12 w-auto">
+    </div>
+    
+    <!-- Nombre de la empresa -->
+    <div class="ml-3">
+        <h1 class="text-2xl font-bold text-gray-900">Bridgely</h1>
+        <p class="text-sm text-gray-600">Connecting Ideas & Futures</p>
+    </div>
+</div>
